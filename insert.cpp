4459f8d472eb71335ad9958fba6321d617234489
terra-ryzing <vedantmafiaboss@gmail.com>
2016-11-22 22:28:38 +0530

    Add files via upload

---
+++ insert.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int arr[10]={2,3,6,9,7,4,1,8,10,5};
+
+  int i,n;
+
+  n=10;
+
+  int holePosition, valueToInsert;
+
+
+      /* select value to be inserted */
+  for(i=1;i<n;i++){
+
+     valueToInsert=arr[i];
+     holePosition=i;
+
+  /*locate hole position for the element to be inserted */
+  while(holePosition>0 && arr[holePosition-1]> valueToInsert){
+
+    arr[holePosition]=arr[holePosition-1];
+    holePosition=holePosition-1;
+    }
+
+   /* insert the number at hole position */
+
+  arr[holePosition]=valueToInsert;
+  }
+
+  for(i=0;i<n;i++){
+    cout<<arr[i]<<",";
+  }
+    return 0;
+}
