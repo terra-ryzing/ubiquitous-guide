a5ce8659da7301ba933afda31a415ed30af15d19
terra-ryzing <vedantmafiaboss@gmail.com>
2016-11-30 14:45:36 +0530

    Update insert.cpp

--- insert.cpp
+++ insert.cpp
@@ -1,37 +1,47 @@
-#include <iostream>
+#include < iostream >
 
-using namespace std;
+  using namespace std;
 
-int main()
-{ int arr[10]={2,3,6,9,7,4,1,8,10,5};
+int main() {
+  int arr[10] = {
+    2,
+    3,
+    6,
+    9,
+    7,
+    4,
+    1,
+    8,
+    10,
+    5
+  };
 
-  int i,n;
+  int i, n;
 
-  n=10;
+  n = 10;
 
   int holePosition, valueToInsert;
 
+  /* select value to be inserted */
+  for (i = 1; i < n; i++) {
 
-      /* select value to be inserted */
-  for(i=1;i<n;i++){
+    valueToInsert = arr[i];
+    holePosition = i;
 
-     valueToInsert=arr[i];
-     holePosition=i;
+    /*locate hole position for the element to be inserted */
+    while (holePosition > 0 && arr[holePosition - 1] > valueToInsert) {
 
-  /*locate hole position for the element to be inserted */
-  while(holePosition>0 && arr[holePosition-1]> valueToInsert){
-
-    arr[holePosition]=arr[holePosition-1];
-    holePosition=holePosition-1;
+      arr[holePosition] = arr[holePosition - 1];
+      holePosition = holePosition - 1;
     }
 
-   /* insert the number at hole position */
+    /* insert the number at hole position */
 
-  arr[holePosition]=valueToInsert;
+    arr[holePosition] = valueToInsert;
   }
 
-  for(i=0;i<n;i++){
-    cout<<arr[i]<<",";
+  for (i = 0; i < n; i++) {
+    cout << arr[i] << ",";
   }
-    return 0;
+  return 0;
 }
